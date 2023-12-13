8992c1b01094f52cbf9da87270438b326bc86d15
Mohammed <saadisaadi768@gmail.com>
2023-12-13 14:53:53 -0600

    bug

--- client/src/components/ColorPicker.jsx
+++ client/src/components/ColorPicker.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { SketchPicker } from 'react-color'
 import { useSnapshot } from 'valtio'
-
 import state from '../store'
 const ColorPicker = () => {
   const snap = useSnapshot(state);
@@ -27,6 +26,30 @@ const ColorPicker = () => {
      onChange={(color) => state.color = color.hex}
      />
     </div>
+  )
+}
+
+export const ColorPickerStatic = () => {
+  return (
+    <SketchPicker 
+     color="#4ccc"
+     disableAlpha
+     presetColors={[
+      "#4ccc",
+      "#EFBD4E",
+      "#80C670",
+      "#726DE8",
+      "#353934",
+      "#2CCCE4",
+      "#ff8a65",
+      "#7098DA",
+      "#C19277",
+      "#FF96AD",
+      "#512314",
+      "#5F123D",
+     ]}
+     onChange={(color) => {}}
+     />
   )
 }
 
